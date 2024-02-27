9019440afee4a2d4ba6361de0f999157713ce4e1
Ola Lindberg <ola.lindberg@ntig.se>
2024-02-27 13:27:06 +0100

    MVC with a HTML view

--- app.rb
+++ app.rb
@@ -4,7 +4,30 @@ require "sinatra/base"
 class App < Sinatra::Base 
 
     get '/' do
-        "Vad är 3+8?"
+        @first = rand(20)
+        @second = rand(20)
+
+        @vanswer = params[:vanswer]
+        @iscorrect = params[:iscorrect]
+
+        puts params
+
+        erb :index
+    end
+
+    post '/validate' do
+        first = params["first"].to_i
+        second = params["second"].to_i
+        answer = params["answer"].to_i
+        puts "Validating: Is #{first} + #{second} = #{answer}?"
+        
+        if first + second == answer
+            puts "Rätt"
+            redirect "/?vanswer=#{answer}&iscorrect=yes"
+        else
+            redirect "/?vanswer=#{answer}&iscorrect=no"
+        end
+
     end
 
 end
